920876846124a5ecb0e46fa7145935f23bf7ea9f
Yuval <yuval.haornoga@automat-it.com>
2021-06-10 16:44:24 +0300

    lambda files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # StopStartEC2
 
-Bash script to Stop or Start an EC2 instance by a name or multiple EC2 instances by tag.
+Two options:
+- Bash script to Stop or Start an EC2 instance by a name or multiple EC2 instances by tag.
+Simply run the file ./StopStartMenu.sh
+
+- Lambda python files in stopstart_lambda folder
---
+++ stopstart_lambda/startInstance.py
@@ -0,0 +1,11 @@
+import boto3
+import pprint
+
+def lambda_handler(event, context):
+    region = "eu-west-1"
+    ec2 = boto3.client('ec2', region_name=region)
+    instanceIDS = ec2.describe_instances(Filters=[{'Name': 'tag:Env', 'Values': ['Lab-1']}])
+
+    for instanceID in instanceIDS['Reservations']:
+        instance = instanceID['Instances'][0]['InstanceId']
+        pprint.pprint(ec2.start_instances(InstanceIds=[instance]))
